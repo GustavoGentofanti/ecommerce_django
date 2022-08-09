da07a312dd6367df417de336bbc972b748995a2b
GustavoGentofanti <gustavogentofanti.pro@gmail.com>
2022-08-09 19:35:19 -0300

    Order models and Order Item models

--- loja/settings.py
+++ loja/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'produto',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'debug_toolbar',
 ]
 
--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+import os
+from PIL import Image
+from django.conf import settings
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255, verbose_name='Nome')
+    descricao_curta = models.TextField(max_length=255, 
+        verbose_name='Descrição curta')
+    descricao_longa = models.TextField(max_length=255, 
+        verbose_name='Descrição longa')
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField(verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0,
+        verbose_name='Preço promocional')
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        width, height = img_pil.size
+        new_height = round((new_width * height) / width)
+
+        if width <= new_width:
+            img_pil.close()
+            return
+
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=60
+        )
+
+        new_img.close()
+
+    def __str__(self):
+        return self.nome
+
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField(verbose_name="Preço")
+    preco_promocional = models.FloatField(default=0,
+         verbose_name="Preço promocional")
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = "Variação"
+        verbose_name_plural = "Variações"
